e1dcf06c86521c06f4b226d1826dc3d645132225
Gustavo Lopes Feitosa <gustavo.2850@aluno.pr.senac.br>
2022-04-27 16:08:55 -0300

    Status dos equipamentos.

--- Produto.java
+++ Produto.java
@@ -3,7 +3,7 @@ package senac2022;
 public class Produto {
 
     private String nome = "Snickers";
-    private String descrição = "Alimento para matar a fome";
+    private String descrição = "Alimento para matar a fome!";
     private double valor = 3.99;
     private double valorNaPromoção = 2.99;
     private String dataDeVencimento = "14/08/2022";
