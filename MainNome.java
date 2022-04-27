9fd3339843de9784b5bb5d4260b5443be126319b
Gustavo Lopes Feitosa <gustavo.2850@aluno.pr.senac.br>
2022-04-27 15:31:28 -0300

    Status dos equipamentos.

--- MainNome.java
+++ MainNome.java
@@ -3,7 +3,7 @@ package senac2022;
 public class MainNome {
 
     public static void main(String[] args) {
-        Nome nome = new Nome();
+        Nome1 nome = new Nome1();
 
         nome.setNome("Lopes");
         nome.setIdade("18");
